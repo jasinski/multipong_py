ef8f6d564bd1a0d8a46315d338f3dbac05812012
Michael Jasinski <michael.jasinski@volvocars.com>
2016-11-26 14:40:19 +0100

    Testing clients array..

---
+++ client.py
@@ -0,0 +1,7 @@
+class Client(object):
+
+    def __init__(self, clientId, ip, port):
+        super(self).__init__()
+        self.clientId = cliendId
+        self.ip = ip
+        self.port = port
--- server.py
+++ server.py
@@ -1,12 +1,17 @@
 import socket
+from client import Client
 
-UDP_IP = "138.68.66.77"
+UDP_IP = "localhost" #"138.68.66.77"
 UDP_PORT = 9876
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
+clients = []
+
+print "Server up n runnin..."
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print "Addr: ", addr
     print "received message:", data
