4b4430b3a224b487bbe9661154684fca76d63f3c
Michael Jasinski <michael.jasinski@volvocars.com>
2016-11-26 14:49:50 +0100

    Testing more client stuff....

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 class Client(object):
 
     def __init__(self, clientId, ip, port):
-        super(self).__init__()
         self.clientId = cliendId
         self.ip = ip
         self.port = port
