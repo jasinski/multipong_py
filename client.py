565116c3692367e26c57da7e4c07c9b1a36ee130
Michael Jasinski <michael.jasinski@volvocars.com>
2016-12-03 18:21:42 +0100

    Managed to send UDP response to client with a game state (x & y coordinates)!

--- client.py
+++ client.py
@@ -1,13 +1,15 @@
 class Client:
 
     def __init__(self, clientId, ip, port):
+        print("init 1 is used")
         self.clientId = clientId
         self.ip = ip
-        self.port = port
+        self.port = int(port)
 
     def __init__(self, address, data):
+        print("init 2 is used")
         self.ip = address[0]
-        self.port = address[0]
+        self.port = int(address[1])
         arr = self.dataStringToArray(data)
         self.clientId = arr[0]
         self.msgType = arr[1]
@@ -27,3 +29,6 @@ class GameState:
         arr = gameStateString.split(",")
         self.x = arr[0]
         self.y = arr[1]
+
+    def getGameStateAsString(self):
+        return self.x + "," + self.y
--- server.py
+++ server.py
@@ -1,18 +1,23 @@
 import socket
 from client import Client, GameState
 
-UDP_IP = "138.68.66.77"
+IP_LOCAL = "192.168.1.72" # Local here is mikes MACBOOK
+IP_SERVER = "138.68.66.77"
+UDP_IP = IP_LOCAL
+
 UDP_PORT = 9876
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
+# All the connecting clients will be held in this array
 clients = list()
 
 print "Server up n runnin..."
 while True:
+    print("Waiting for client data...")
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print("Handling client...")
 
     #ip = addr[0]
     #port = addr[1]
@@ -26,10 +31,17 @@ while True:
     if foundClient is False:
         clients.append(client)
 
-
+    # Printing some relevant info about clients
     for index, client in enumerate(clients):
         print("Client ", index, " info:")
         print(client.clientId)
         print(client.ip)
         print(client.port)
         print("(x: ", client.currentGameState.x, "y: ", client.currentGameState.y, ")")
+
+
+    #sock.sendto("THIS IS A MESSAGE FROM SPARTA", (clients[0].ip, 61991))
+    for index, client in enumerate(clients):
+        #sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+        clientIp = client.ip
+        sock.sendto(client.currentGameState.getGameStateAsString(), (clientIp, 61991))
